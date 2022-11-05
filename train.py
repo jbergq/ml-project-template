af6916ca6a5797cf39599685b6f9357617400977
Jonathan Bergqvist <jonathan@seednode.io>
2022-11-05 15:59:56 +0100

    Add type hints to train.py

--- train.py
+++ train.py
@@ -1,14 +1,15 @@
 import hydra
 from hydra.utils import instantiate
 from omegaconf import DictConfig
+from pytorch_lightning import LightningDataModule, LightningModule, Trainer
 
 
 @hydra.main(config_path="config", config_name="config")
-def main(cfg: DictConfig):
+def main(cfg: DictConfig) -> None:
     # Instantiate modules and trainer.
-    datamodule = instantiate(cfg.datamodule)
-    model = instantiate(cfg.model)
-    trainer = instantiate(cfg.trainer)
+    datamodule: LightningDataModule = instantiate(cfg.datamodule)
+    model: LightningModule = instantiate(cfg.model)
+    trainer: Trainer = instantiate(cfg.trainer)
 
     # Start the training.
     trainer.fit(model, datamodule)
